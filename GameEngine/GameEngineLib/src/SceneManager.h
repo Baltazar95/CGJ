946b02690f41bc1241599bbe3496557c477e84ca
telmamnb <telmoquinhas@hotmail.com>
2017-12-12 16:04:49 +0000

    [CHANGE] Conecting Materials with Meshes and Scene Manager

--- GameEngine/GameEngineLib/src/SceneManager.h
+++ GameEngine/GameEngineLib/src/SceneManager.h
@@ -8,6 +8,7 @@
 #include "Camera.h"
 #include "KeyBuffer.h"
 
+
 class SceneManager
 {
 	private:
@@ -17,6 +18,7 @@ class SceneManager
 		Vector3 position = Vector3(0.0f, 0.0f, 0.0f);
 		SceneNode *tangram, *base, *cube, *pyramide, *smallPyramideLeft, *smallPyramideRight, *bigPyramideLeft, *bigPyramideRight, *parallelepiped;
 		MatrixFactory mf;
+
 	public:
 		SceneManager();
 		~SceneManager();
