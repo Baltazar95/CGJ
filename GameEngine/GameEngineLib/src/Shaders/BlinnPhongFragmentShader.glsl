fc29352ac4423cd6a1c4c48071f9d06c7e8bd7f1
Baltazar95 <tiago_mpb95@hotmail.com>
2017-12-13 13:56:29 +0000

    [UPDATE] removing test

--- GameEngine/GameEngineLib/src/Shaders/BlinnPhongFragmentShader.glsl
+++ GameEngine/GameEngineLib/src/Shaders/BlinnPhongFragmentShader.glsl
@@ -36,6 +36,6 @@ void main(void)
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
 	vec3 specular = specularStrength * spec * lightColor;
 
-	vec3 result = (/*ambient + diffuse +*/ specular) * objectColor;
+	vec3 result = (ambient + diffuse + specular) * objectColor;
 	FragmentColor = vec4(result, 1.0);
 }
